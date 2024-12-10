ce9cfa748f5709f90e575f86c7ece365d876aaa3
Aleksandr Kobzev <alexsandkobz98@gmail.com>
2024-12-10 15:10:42 +0300

    Фильтрация теперь работает через сервер, добавлен слайдер фотографий достопримечательностей

--- src/js/page2.js
+++ src/js/page2.js
@@ -1,4 +1,46 @@
-// Функция для создания элементов DOM
+async function fetchFilteredCards(filters) {
+    const url = new URL('https://672b185d976a834dd02595f5.mockapi.io/cards');
+    Object.keys(filters).forEach(key => {
+        if (filters[key]){
+            url.searchParams.append(key, filters[key]);
+        }
+    })
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { 'content-type': 'application/json' } 
+        })
+
+        if (!response.ok){
+            throw error = new Error('Ошибка при получении данных!');
+        }
+
+        cards = await response.json();
+        displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
+        setupPagination();
+        displayFilters(getType(cards));
+
+    } catch {
+        console.log('error: ' + error.message)
+    }
+}
+
+const filtersDiv = document.getElementById("filters");
+
+filtersDiv.addEventListener('change', (event) => {
+    const filters = {};
+
+    document.querySelectorAll('.second__functional-checkbox').forEach(input => {
+        if (input.checked) {
+            filters[input.id] = input.value;
+        }
+    });
+
+    fetchFilteredCards(filters);
+});
+
+
 function makeDiv(className) {
     const div = document.createElement('div');
     div.classList.add(className);
@@ -25,7 +67,7 @@ function makeCard(card) {
     itemCard.src = card.card;
     itemCard.alt = card.name;
     itemCard.id = card.name;
-    itemCard.onclick = () => openDetails(card.UniqNum);
+    itemCard.onclick = () => openDetails(card.id);
     itemCard.classList.add('second__card-img');
     return itemCard;
 }
@@ -33,6 +75,134 @@ function makeCard(card) {
 function getType(data) {
     return data.map(card => card.filter);
 }
+
+// пагинация
+const itemsPerPage = 4;
+let page = 1;
+let cards = [];
+let isLoading = false;
+
+async function fetchPagination(Error) {
+    const url = new URL('https://672b185d976a834dd02595f5.mockapi.io/card');
+    url.searchParams.append('completed', false);
+    url.searchParams.append('page', 1);
+    url.searchParams.append('limit', 10);
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {'content-type':'application/json'},
+    })
+
+    try {
+        if (!response.ok){
+            return
+        }
+
+        cards = await response.json();
+        displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
+        setupPagination();
+        displayFilters(getType(cards));
+    } catch {
+        console.log('error: ' + Error.message)
+    }
+}
+
+
+
+async function fetchCards() {
+    try {
+        const response = await fetch('https://672b185d976a834dd02595f5.mockapi.io/cards');
+
+        if (!response.ok) {
+            throw new Error('Ошибка при получении данных!');
+        }
+
+        cards = await response.json();
+        displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
+        setupPagination();
+        displayFilters(getType(cards));
+    } catch (error) {
+        console.log('error: ' + error.message);
+    }
+}
+
+const cardsPage = document.querySelector('.second__page');
+
+
+function displayCards(cards) {
+    cardsPage.innerHTML = '';
+
+    const cardsUl = document.createElement('ul');
+    cardsUl.classList.add('second__card-list');
+
+    cards.forEach(card => {
+        cardsUl.appendChild(makeCard(card));
+    });
+
+    cardsPage.appendChild(cardsUl);
+}
+
+const uniqueFilters = new Set();
+
+function displayFilters(filters) {
+
+    filters.forEach(filter => {
+        const filterElement = makeDiv("second__functional-filter");
+
+        const input = makeInput('checkbox', 'firstCheckbox', 'second__functional-checkbox');
+        const p = makeP('second__functional-text', filter.text);
+
+        if (uniqueFilters.has(filter.type)) {
+            return;
+        } else {
+            uniqueFilters.add(filter.type);
+        }
+
+        filterElement.appendChild(input);
+        filterElement.appendChild(p);
+
+        input.addEventListener('change', function () {
+            if (input.checked) {
+                const firstTargetCards = cards.filter(item1 => item1.filter.type.toLowerCase() === filter.type.toLowerCase());
+                if (firstTargetCards.length > 0) {
+                    paginBtn.style.display = 'none';
+                    displayCards(firstTargetCards);
+                } else {
+                    console.log('Карточек для чекбокса не найдено!');
+                }
+            } else {
+                paginBtn.style.display = 'flex';
+                displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
+                displayFilters(getType(cards));
+            }
+        });
+
+        filtersDiv.appendChild(filterElement);
+    });
+}
+
+const paginBtn = document.querySelector('.second__pagination');
+function setupPagination() {
+    const totalPages = Math.ceil(cards.length / itemsPerPage);
+
+    paginBtn.innerHTML = '';
+    if (totalPages > 1) {
+        for (let i = 1; i <= totalPages; i++) {
+            const pageButton = document.createElement('button');
+            pageButton.className = 'second__pagination-link';
+            pageButton.textContent = i;
+            pageButton.addEventListener('click', () => {
+                page = i;
+                displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
+            });
+            paginBtn.appendChild(pageButton);
+        }
+    } else {
+        paginBtn.style.display = 'none';
+    }
+}
+
+fetchCards();
 
 // Функция поиска
 const searchInput = document.getElementById('search');
@@ -98,7 +268,6 @@ sortOptions.onclick = function () {
         : (optionsList.classList.remove('second__functional-list-open'), optionsList.classList.add('second__functional-list-close'));
 };
 
-// Функция для отправки запроса с параметрами сортировки
 async function fetchSortedCards(sortBy, order = 'asc') {
     const url = new URL('https://672b185d976a834dd02595f5.mockapi.io/cards');
     url.searchParams.append('sortBy', sortBy);
@@ -113,9 +282,8 @@ async function fetchSortedCards(sortBy, order = 'asc') {
         if (!response.ok) {
             throw new Error('Ошибка при получении данных!');
         }
-
         cards = await response.json();
-        console.log(cards); 
+        console.log(cards)
         displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
         setupPagination();
         displayFilters(getType(cards));
@@ -130,7 +298,7 @@ const firstSortingClose = document.querySelector('.second__functional-sorting_cl
 
 firstSortingDiv.onclick = function () {
     firstSortingClose.style.display === "block" ? 
-        (secondSortingDiv.style.display = 'flex', thirdSortingDiv.style.display = 'flex', firstSortingClose.style.display = 'none', fetchSortedCards('id', 'asc')) :
+        (secondSortingDiv.style.display = 'flex', thirdSortingDiv.style.display = 'flex', firstSortingClose.style.display = 'none', fetchSortedCards('-UniqNum', 'asc')) :
         (firstSortingClose.style.display = 'block', secondSortingDiv.style.display = 'none', thirdSortingDiv.style.display = 'none', fetchSortedCards('popularity', 'desc'));
 };
 
@@ -143,7 +311,7 @@ secondSortingDiv.onclick = function () {
     thirdSortingDiv.style.display = isOpenSecond ? 'flex' : 'none';
     secondSortingClose.style.display = isOpenSecond ? 'none' : 'block';
 
-    fetchSortedCards(isOpenSecond ? 'id' : 'name', 'asc');
+    fetchSortedCards(isOpenSecond ? '-UniqNum' : 'name', 'asc');
 };
 
 const thirdSortingDiv = document.querySelector('.second__functional-sorting_3');
@@ -156,134 +324,34 @@ thirdSortingDiv.onclick = function () {
     firstSortingDiv.style.display = isOpenThird ? 'flex' : 'none';
     thirdSortingClose.style.display = isOpenThird ? 'none' : "block";
 
-    fetchSortedCards(isOpenThird ? 'id' : 'name', 'desc');
+    fetchSortedCards(isOpenThird ? '-UniqNum' : 'name', 'desc');
 };
 
-// пагинация
-const itemsPerPage = 4;
-let page = 1;
-let cards = [];
-let isLoading = false;
-
-async function fetchCards() {
-    try {
-        const response = await fetch('https://672b185d976a834dd02595f5.mockapi.io/cards');
-
-        if (!response.ok) {
-            throw new Error('Ошибка при получении данных!');
-        }
-
-        cards = await response.json();
-        displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
-        setupPagination();
-        displayFilters(getType(cards));
-    } catch (error) {
-        console.log('error: ' + error.message);
-    }
-}
-
-const cardsPage = document.querySelector('.second__page');
-
-function displayCards(cards) {
-    cardsPage.innerHTML = '';
-
-    const cardsUl = document.createElement('ul');
-    cardsUl.classList.add('second__card-list');
-
-    cards.forEach(card => {
-        cardsUl.appendChild(makeCard(card));
-    });
-
-    cardsPage.appendChild(cardsUl);
-}
-
-const filtersDiv = document.getElementById("filters");
-
-const uniqueFilters = new Set();
-
-function displayFilters(filters) {
-    filtersDiv.innerHTML = '';
-
-    filters.forEach(filter => {
-        const filterElement = makeDiv("second__functional-filter");
-
-        const input = makeInput('checkbox', 'firstCheckbox', 'second__functional-checkbox');
-        const p = makeP('second__functional-text', filter.text);
-
-        if (uniqueFilters.has(filter.type)) {
-            return;
-        } else {
-            uniqueFilters.add(filter.type);
-        }
-
-        filterElement.appendChild(input);
-        filterElement.appendChild(p);
-
-        input.addEventListener('change', function () {
-            if (input.checked) {
-                const firstTargetCards = cards.filter(item1 => item1.filter.type.toLowerCase() === filter.type.toLowerCase());
-                if (firstTargetCards.length > 0) {
-                    paginBtn.style.display = 'none';
-                    displayCards(firstTargetCards);
-                } else {
-                    console.log('Карточек для чекбокса не найдено!');
-                }
-            } else {
-                paginBtn.style.display = 'flex';
-                displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
-            }
-        });
-
-        filtersDiv.appendChild(filterElement);
-    });
-}
-
-const paginBtn = document.querySelector('.second__pagination');
-function setupPagination() {
-    const totalPages = Math.ceil(cards.length / itemsPerPage);
-
-    paginBtn.innerHTML = '';
-    if (totalPages > 1) {
-        for (let i = 1; i <= totalPages; i++) {
-            const pageButton = document.createElement('button');
-            pageButton.className = 'second__pagination-link';
-            pageButton.textContent = i;
-            pageButton.addEventListener('click', () => {
-                page = i;
-                displayCards(cards.slice((page - 1) * itemsPerPage, page * itemsPerPage));
-            });
-            paginBtn.appendChild(pageButton);
-        }
-    } else {
-        paginBtn.style.display = 'none';
-    }
-}
-
-fetchCards();
 
 const functional = document.querySelector('.second__functional-header');
-function openDetails(itemUniqNum) {
-    window.location.href = `?id=${itemUniqNum}`;
+function openDetails(id) {
+    window.location.href = `?id=${id}`;
 }
 
 async function fetchSights() {
-    const response = await fetch("https://67444af2b4e2e04abea198bc.mockapi.io/cards/Attractions");
+    const response = await fetch("https://672b185d976a834dd02595f5.mockapi.io/cards");
     return response.json();
 }
+
+const secondPage = document.querySelector('.second')
 
 function renderSights(sights) {
     sights.forEach(sight => {
         const attrDiv = document.createElement('div');
         attrDiv.classList.add("details__card");
+
         cardsPage.appendChild(attrDiv);
     });
 }
 
-
-
 async function showDetails(sightId) {
     const sights = await fetchSights();
-    const sight = sights.find(a => a.UniqNum === sightId);
+    const sight = sights.find(a => a.id === sightId);
     if (sight) {
         cardsPage.style.display = 'none';
         functional.style.display = 'none';
@@ -291,24 +359,34 @@ async function showDetails(sightId) {
 
         const detailsDiv = document.querySelector('.details');
         detailsDiv.innerHTML = `
+            <div class='details__images-wrapper'> 
+                <div class='details__images'> 
+                    <p class='details__slider-btn details__previus' id='back'> &larr; </p>
+                    <img class ='details__slider-img' src='${sight.images[0]}' alt='img'>
+                    <p class='details__slider-btn details__next' id='next'> &rarr; </p>
+                </div>
+            </div>
             <a href="#" onclick="history.back()"> ← Назад </a>
             <div class="details__block">            
                 <h1>${sight.name}</h1>
                 <div class='details__details-part'>
-                    <p class="details__text">${sight.About[2]}</p>
-                    <img src="${sight.About[0]}" alt="Image" class="details__img">
+                    <p class="details__text">${sight.About[0]}</p>
+                    <img src="${sight.images[0]}" alt="Image" onclick='openImagesSlider()' class="details__img">
                 </div>
                 <div class='details__details-part adaptiv'>
-                    <img src="${sight.About[1]}" alt="Image">
-                    <p>${sight.About[3]}</p>
+                    <img src="${sight.images[1]}" onclick='openImagesSlider()' alt="Image">
+                    <p class="details__text">${sight.About[1]}</p>
                 </div>
                 <h2>Достопримечательность на карте</h2>
                 <iframe src="${sight.Map}" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                
+
+
                 <h2>Отзывы</h2>
                 <div class='details__createComment' id='createComment'> 
                     <form class='details__inputs'>
-                        <input placeholder='Ваше имя' class='details__nameInput' id="reviewName">
-                        <textarea placeholder='Введите сообщение' class='details__textInput' id="reviewText"></textarea>
+                        <input minlength='2' maxlength="33" placeholder='Ваше имя' class='details__nameInput' id="reviewName" required>
+                        <textarea maxlength="650" placeholder='Введите сообщение' class='details__textInput' id="reviewText" required></textarea>
                     </form>
                     <div class='details__hr' id='detailsHr'> </div>
                     <div class='details__buttons'>
@@ -347,13 +425,13 @@ async function showDetails(sightId) {
         detailsDiv.classList.remove('hidden');
 
 
-        loadReviews(sight.UniqNum);
+        loadReviews(sight.id);
 
 
         document.getElementById('submitReview').addEventListener('click', function(event) {
             event.preventDefault();
 
-            const attractionId = sight.UniqNum;
+            const attractionId = sight.id;
             const reviewName = document.getElementById('reviewName').value;
             const reviewText = document.getElementById('reviewText').value;
 
@@ -449,3 +527,47 @@ let options =  {
     hour: 'numeric', 
     minute: 'numeric' 
 }
+
+const body = document.body
+function openImagesSlider(sight) {
+    const imagesSlider = document.querySelector('.details__images-wrapper');
+    if (imagesSlider) {
+        imagesSlider.style.display = 'flex'; 
+        body.style.overflowY = 'hidden'; 
+
+
+        imagesSlider.addEventListener('click', function (event) {
+            if (event.target === imagesSlider) {
+                imagesSlider.style.display = 'none'; 
+                body.style.overflowY = 'auto'; 
+            }
+        });
+
+        const sliderNext = imagesSlider.querySelector('#next');
+        const sliderPrev = imagesSlider.querySelector('#back');
+        if (sliderNext && sliderPrev) {
+            let currentImageIndex = 0;
+
+            sliderNext.addEventListener('click', function () {
+                const imgElement = imagesSlider.querySelector('.details__slider-img');
+
+                currentImageIndex = (currentImageIndex + 1) % sight.images.length;
+
+                imgElement.src = sight.images[currentImageIndex];
+            });
+
+            sliderPrev.addEventListener('click', function () {
+                const imgElement = imagesSlider.querySelector('.details__slider-img');
+
+
+                currentImageIndex = (currentImageIndex + 1 + sight.images.length) % sight.images.length;
+                imgElement.src = sight.images[currentImageIndex];
+            });
+        } else {
+            console.error('Элементы #next или #back не найдены');
+        }
+    } else {
+        console.error('Элемент .details__images-wrapper не найден');
+    }
+}
+
